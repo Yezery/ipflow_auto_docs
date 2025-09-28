1520d11bbb176cb32e2e5d703768904d19e20eb6
正睿 叶 <1172029997@qq.com>
2025-09-28 11:13:12 +0800

    deploy

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,58 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "IPFLOW Automation",
+  description: "IPFLOW 日活自动化使用说明",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: "/icon.svg",
+
+    nav: [
+      { text: "主页", link: "/" },
+      { text: "快速开始", link: "/快速开始" },
+      { text: "Q&A", link: "/Q&A" },
+    ],
+
+    sidebar: [
+      {
+        text: "使用说明",
+        collapsed: true,
+        items: [
+          { text: "快速开始", link: "/快速开始" },
+          { text: "Q&A", link: "/Q&A" },
+        ],
+      },
+    ],
+
+    // socialLinks: [
+    //   { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    // ]
+    lastUpdated: true,
+    lastUpdatedText: "最后更新",
+
+    search: {
+      provider: "local",
+      options: {
+        locales: {
+          zh: {
+            translations: {
+              button: {
+                buttonText: "搜索文档",
+                buttonAriaLabel: "搜索文档",
+              },
+              modal: {
+                noResultsText: "无法找到相关结果",
+                resetButtonTitle: "清除查询条件",
+                footer: {
+                  selectText: "选择",
+                  navigateText: "切换",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,27 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "IPFLOW Automation"
+  text: "使用说明"
+  tagline: 版本 V1.2.0
+  actions:
+    - theme: brand
+      text: 开始使用
+      link: /快速开始
+
+
+features:
+  - title: 自动导入钱包数据
+    details: 支持导入 MetaMask 钱包插件、自动导入钱包助记词还原钱包
+  - title: 自动完成签到
+    details: 自动跳转个人主页 完成签到
+  - title: 自动修改个人信息
+    details: 头像自动选择上传、AI 生成昵称
+  - title: 自动刷剧
+    details: 可设置刷剧时间、AI 评论生成与自动发表
+  - title: 浏览器无痕多开
+    details: 自动推荐并发量、浏览器数据隔离
+---
+
